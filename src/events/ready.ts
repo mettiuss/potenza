76dce6b25f460625e92a5190d2695d83594540fc
Mettius <69057048+MettiusHyper@users.noreply.github.com>
2022-10-26 12:41:16 +0200

    change status 💬

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,7 +1,8 @@
-import { Client } from 'discord.js';
+import { Client, PresenceUpdateStatus } from 'discord.js';
 
 export const name = 'ready';
 export const once = true;
 export async function execute(client: Client) {
+	client.user!.setStatus(PresenceUpdateStatus.DoNotDisturb);
 	console.log(`Ready! Logged in as ${client.user!.tag}`);
 }
