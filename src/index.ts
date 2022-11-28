899a83c10fb7d1b60acbe87a38da7ef378db3aab
Mettius <69057048+MettiusHyper@users.noreply.github.com>
2022-11-28 23:38:58 +0100

    express server

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,10 @@ import slashInit from './deploy-commands.js';
 client.login(process.env.TOKEN);
 
 await slashInit();
+
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+app.listen(process.env.PORT, () => console.log('listening'));
