cb58d30f3d6be7a07bc5de20599269b8acc730b0
Metteo <69057048+mettiuss@users.noreply.github.com>
2023-10-18 12:03:27 +0200

    remove old events folder

--- src/events/messageCreate.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from 'discord.js';
-
-export const name = 'messageCreate';
-export const once = false;
-export async function execute(message: Message) {
-	if (message.author.id === '710078958036582471' && message.channel.id === '602075899549974539')
-		await message.delete();
-}
--- src/events/ready.ts
+++
@@ -1,9 +0,0 @@
-import { Client, PresenceUpdateStatus, TextChannel } from 'discord.js';
-
-export const name = 'ready';
-export const once = true;
-export async function execute(client: Client) {
-	client.user!.setStatus(PresenceUpdateStatus.DoNotDisturb);
-	client.log_channel = (await client.channels.fetch(process.env.LOG_CHANNEL!)) as TextChannel;
-	console.log(`Ready! Logged in as ${client.user!.tag}`);
-}
