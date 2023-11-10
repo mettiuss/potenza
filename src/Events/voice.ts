c37b93c4a05f7f4ea29184caccf0b11c216638b6
Metteo <69057048+mettiuss@users.noreply.github.com>
2023-11-10 20:46:05 +0000

    Rimozione log e implementazione di variabili .env

--- src/Events/voice.ts
+++ src/Events/voice.ts
@@ -4,14 +4,9 @@ export const name = 'voiceStateUpdate';
 export const once = false;
 
 export async function execute(oldState: VoiceState, newState: VoiceState) {
-  const channelID = '1164249961093926974'; // ID MAIN CHANNEL
+  if (!newState.channel || !newState.member || newState.channelId !== process.env.CREATE_CHANNEL_ID) return;
 
-  if (!newState.member || newState.channelId !== channelID || !newState.channel) return;
-
-  const user = newState.member.user;
-  const newChannelName = `${user.username}'s channel`;
-
-  console.log('Creating voice channel...');
+  const newChannelName = `${newState.member.user.username}'s channel`;
 
   const newChannel = await newState.guild!.channels.create({
     name: newChannelName,
@@ -28,9 +23,6 @@ export async function execute(oldState: VoiceState, newState: VoiceState) {
       },
     ],
   });
-
-    console.log('Channel created:', newChannel);
-    newState.setChannel(newChannel);
-    console.log('User moved to the channel');
+  await newState.setChannel(newChannel);
 }
 
