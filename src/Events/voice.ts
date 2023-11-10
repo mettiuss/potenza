9907c3d47eec56c2761b1d3623662a6638e72c91
Mirko <133505161+mirkohubtv@users.noreply.github.com>
2023-11-10 19:35:00 +0100

    Checks

--- src/Events/voice.ts
+++ src/Events/voice.ts
@@ -6,40 +6,31 @@ export const once = false;
 export async function execute(oldState: VoiceState, newState: VoiceState) {
   const channelID = '1164249961093926974'; // ID MAIN CHANNEL
 
-  // Verifica se il membro è in un canale vocale specifico e se ha il canale principale
-  if (!newState.member || newState.channelId !== channelID) return;
+  if (!newState.member || newState.channelId !== channelID || !newState.channel) return;
 
   const user = newState.member.user;
   const newChannelName = `${user.username}'s channel`;
 
   console.log('Creating voice channel...');
 
-  const parentChannel = newState.channel;
-
-  // Se non c'è il canale principale, esce dalla funzione
-  if (!parentChannel) return;
-
-  try {
-    const newChannel = await newState.guild!.channels.create({
-      name: newChannelName,
-      type: ChannelType.GuildVoice,
-      parent: parentChannel.parent,
-      permissionOverwrites: [
-        {
-          id: newState.guild!.id,
-          allow: [PermissionFlagsBits.ViewChannel],
-        },
-        {
-          id: newState.member.id,
-          allow: [PermissionFlagsBits.Connect, PermissionFlagsBits.ViewChannel, PermissionFlagsBits.Speak, PermissionFlagsBits.ManageChannels],
-        },
-      ],
-    });
+  const newChannel = await newState.guild!.channels.create({
+    name: newChannelName,
+    type: ChannelType.GuildVoice,
+    parent: newState.channel.parent,
+    permissionOverwrites: [
+      {
+        id: newState.guild!.id,
+        allow: [PermissionFlagsBits.ViewChannel],
+      },
+      {
+        id: newState.member.id,
+        allow: [PermissionFlagsBits.Connect, PermissionFlagsBits.ViewChannel, PermissionFlagsBits.Speak, PermissionFlagsBits.ManageChannels],
+      },
+    ],
+  });
 
     console.log('Channel created:', newChannel);
     newState.setChannel(newChannel);
     console.log('User moved to the channel');
-  } catch (error) {
-    console.error('Error:', error);
-  }
 }
+
