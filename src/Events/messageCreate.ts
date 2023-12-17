ca3b41641d0e13d5ee65c9b79b8b4098dce0641b
Metteo <69057048+mettiuss@users.noreply.github.com>
2023-12-17 17:52:11 +0100

    add ping command

--- src/Events/messageCreate.ts
+++ src/Events/messageCreate.ts
@@ -5,4 +5,7 @@ export const once = false;
 export async function execute(message: Message) {
 	if (message.author.id === '710078958036582471' && message.channel.id === '602075899549974539')
 		await message.delete();
+	if (message.author.id === '707165674845241344' && message.content === 'ping') {
+		await message.channel.send(`Pong! ${message.client.ws.ping}`);
+	}
 }
